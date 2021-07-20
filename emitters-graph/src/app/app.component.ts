db498b8c60685614d08d4a67bf728e37e8375b78
Manu <manu.co9913@gmail.com>
2021-07-20 09:33:48 +0300

    First working chart

--- emitters-graph/src/app/app.component.ts
+++ emitters-graph/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'emitters-graph';
+  type = 'line';
+  data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+  options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
 }
