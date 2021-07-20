83712455077914e752516c01ff27b2335e2b815a
Manu <manu.co9913@gmail.com>
2021-07-20 15:35:08 +0300

    refreshing data every three seconds

--- emitters-graph/src/app/bar-chart/bar-chart.component.ts
+++ emitters-graph/src/app/bar-chart/bar-chart.component.ts
@@ -7,6 +7,7 @@ const MAX_FREQ = 40;
 const MIN_FREQ = 0;
 const MAX_ANGLE = 180;
 const MIN_ANGLE = -180;
+const DATA_REFRESHING_TIMEOUT = 3000;
 
 @Component({
   selector: 'app-bar-chart',
@@ -16,29 +17,28 @@ const MIN_ANGLE = -180;
 export class BarChartComponent implements AfterViewInit {
 
   @ViewChild('barCanvas') private barCanvas: ElementRef;
-  barChart: any;
+  barChart: Chart;
 
   constructor() { }
 
   ngAfterViewInit(): void {
     this.barChartMethod();
-
   }
 
-  createRandNumOfEmitters() {
-    let emittersArrData: { x: number, y: number, r: number }[] = [];
+  createRandNumOfEmitters = () => {
+    this.barChart.data.datasets[0].data = [];
 
     for (let i = 1; i < Math.floor(Math.random() * MAX_EMITTERS_IN_GRAPH); i++) {
       let xVal = Math.floor(Math.random() * MAX_ANGLE);
       let xSign = Math.random() > 0.5 ? 1 : -1;
 
-      emittersArrData.push({
+      this.barChart.data.datasets[0].data.push({
         x: xSign * xVal,
         y: Math.random() * MAX_FREQ,
         r: EMITTERS_RADIUS
       })
     }
-    return emittersArrData;
+    this.barChart.update();
   }
 
   barChartMethod() {
@@ -47,7 +47,7 @@ export class BarChartComponent implements AfterViewInit {
       data: {
         datasets: [{
           label: 'First Dataset',
-          data: this.createRandNumOfEmitters(),
+          data: [],
           backgroundColor: 'rgb(0, 99, 132)',
           borderColor: 'rgba(0, 0, 0)',
           borderWidth: 1,
@@ -67,7 +67,8 @@ export class BarChartComponent implements AfterViewInit {
       }
     });
 
-    console.log(this.barChart);
+    setInterval(this.createRandNumOfEmitters, DATA_REFRESHING_TIMEOUT);
+
 
   }
 }
