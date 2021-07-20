9788a92d04662642d9a70cd809f54529e6b3c497
Manu <manu.co9913@gmail.com>
2021-07-20 12:00:21 +0300

    fixed axes of y and x

--- emitters-graph/src/app/bar-chart/bar-chart.component.ts
+++ emitters-graph/src/app/bar-chart/bar-chart.component.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import Chart from 'chart.js/auto';
 
-const EMITTERS_RADIUS = 15;
+const EMITTERS_RADIUS = 10;
 const MAX_FREQ = 40;
-const MAX_ANGLE = 360;
+const MAX_ANGLE = 180;
+const MIN_ANGLE = -180;
 
 @Component({
   selector: 'app-bar-chart',
@@ -15,7 +16,7 @@ export class BarChartComponent implements AfterViewInit {
   @ViewChild('barCanvas') private barCanvas: ElementRef;
   barChart: any;
 
-  emittersArrData: {x:number, y:number, r:number}[] = [];
+  emittersArrData: { x: number, y: number, r: number }[] = [];
 
   constructor() { }
 
@@ -26,9 +27,12 @@ export class BarChartComponent implements AfterViewInit {
   }
 
   createRandNumOfEmitters() {
-    for( let i = 1; i < Math.floor(Math.random() * 15); i++){
+    for (let i = 1; i < Math.floor(Math.random() * 15); i++) {
+      let xVal = Math.floor(Math.random() * MAX_ANGLE);
+      let xSign = Math.random() > 0.5 ? 1 : -1;
+
       this.emittersArrData.push({
-        x: Math.floor(Math.random() * MAX_ANGLE),
+        x: xSign * xVal,
         y: Math.random() * MAX_FREQ,
         r: EMITTERS_RADIUS
       })
@@ -45,9 +49,21 @@ export class BarChartComponent implements AfterViewInit {
           backgroundColor: 'rgb(0, 99, 132)'
         }]
       },
+      options: {
+        scales: {
+          x: {
+            max: 180,
+            min: -180,
+          },
+          y: {
+            max: 40,
+            min: 0,
+          }
+        }
+      }
     });
 
     console.log(this.barChart);
-    
+
   }
 }
