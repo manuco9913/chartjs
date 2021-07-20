95804ff7f04db2712a3cdf2b68605735556d4c95
Manu <manu.co9913@gmail.com>
2021-07-20 11:37:58 +0300

    randomized dots on the graph

--- emitters-graph/src/app/bar-chart/bar-chart.component.ts
+++ emitters-graph/src/app/bar-chart/bar-chart.component.ts
@@ -1,6 +1,10 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import Chart from 'chart.js/auto';
 
+const EMITTERS_RADIUS = 15;
+const MAX_FREQ = 40;
+const MAX_ANGLE = 360;
+
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
@@ -11,37 +15,34 @@ export class BarChartComponent implements AfterViewInit {
   @ViewChild('barCanvas') private barCanvas: ElementRef;
   barChart: any;
 
+  emittersArrData: {x:number, y:number, r:number}[] = [];
+
   constructor() { }
 
   ngAfterViewInit(): void {
+    this.createRandNumOfEmitters();
     this.barChartMethod();
+
+  }
+
+  createRandNumOfEmitters() {
+    for( let i = 1; i < Math.floor(Math.random() * 15); i++){
+      this.emittersArrData.push({
+        x: Math.floor(Math.random() * MAX_ANGLE),
+        y: Math.random() * MAX_FREQ,
+        r: EMITTERS_RADIUS
+      })
+    }
   }
 
   barChartMethod() {
     this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
+      type: 'bubble',
       data: {
-        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
         datasets: [{
-          label: '# of Votes',
-          data: [200, 50, 30, 15, 20, 34],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+          label: 'First Dataset',
+          data: this.emittersArrData,
+          backgroundColor: 'rgb(0, 99, 132)'
         }]
       },
     });
