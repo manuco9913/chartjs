fbbe370ff0be549144fc695de9548640a662bed1
Manu <manu.co9913@gmail.com>
2021-10-17 17:23:40 +0300

    second commit of monorepo husky

--- emitters-graph/.eslintrc.js
+++ emitters-graph/.eslintrc.js
@@ -1,46 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
-        'plugin:jsx-a11y/strict',
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
     },
-    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
-    rules: {
-        'react-hooks/exhaustive-deps': 'error',
-        'no-var': 'error',
-        'brace-style': 'error',
-        'prefer-template': 'error',
-        radix: 'error',
-        'space-before-blocks': 'error',
-        'import/prefer-default-export': 'off',
-    },
-    overrides: [
-        {
-            files: [
-                '**/*.test.js',
-                '**/*.test.jsx',
-                '**/*.test.tsx',
-                '**/*.spec.js',
-                '**/*.spec.jsx',
-                '**/*.spec.tsx',
-            ],
-            env: {
-                jest: true,
-            },
-        },
+    "plugins": [
+        "@typescript-eslint"
     ],
+    "rules": {
+    }
 };
