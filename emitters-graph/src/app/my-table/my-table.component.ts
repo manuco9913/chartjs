7db6388e259c89569d878461bc5679309b415bda
Manu <manu.co9913@gmail.com>
2021-10-18 10:01:35 +0300

    second commit of monorepo husky

--- emitters-graph/src/app/my-table/my-table.component.ts
+++ emitters-graph/src/app/my-table/my-table.component.ts
@@ -5,29 +5,26 @@ import { MatTable } from '@angular/material/table';
 import { MyTableDataSource, MyTableItem } from './my-table-datasource';
 
 @Component({
-  selector: 'app-my-table',
-  templateUrl: './my-table.component.html',
-  styleUrls: ['./my-table.component.css'],
+    selector: 'app-my-table',
+    templateUrl: './my-table.component.html',
+    styleUrls: ['./my-table.component.css'],
 })
 export class MyTableComponent implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort
-  @ViewChild(MatTable) table!: MatTable<MyTableItem>;
-  dataSource: MyTableDataSource;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+    @ViewChild(MatTable) table!: MatTable<MyTableItem>;
+    dataSource: MyTableDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+    /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+    displayedColumns = ['id', 'name'];
 
-  constructor() {
-    this.dataSource = new MyTableDataSource();
-  }
+    constructor() {
+        this.dataSource = new MyTableDataSource();
+    }
 
-  ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator
-     =
-      this.paginator
-      ;
-    this.table.dataSource = this.dataSource;
-  }
+    ngAfterViewInit(): void {
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.table.dataSource = this.dataSource;
+    }
 }
