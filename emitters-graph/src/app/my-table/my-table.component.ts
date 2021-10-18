4f0487c0e899ec01f0efbfa8831b9f2b81ad9f96
Manu <manu.co9913@gmail.com>
2021-10-18 10:03:06 +0300

    last commit of monorepo husky

--- emitters-graph/src/app/my-table/my-table.component.ts
+++ emitters-graph/src/app/my-table/my-table.component.ts
@@ -7,9 +7,9 @@ import { MyTableDataSource, MyTableItem } from './my-table-datasource';
 @Component({ selector: 'app-my-table', templateUrl: './my-table.component.html', styleUrls: ['./my-table.component.css'],
 })export class MyTableComponent implements AfterViewInit {
     @ViewChild(MatPaginator) paginator!: MatPaginator;
-    @ViewChild(MatSort) sort!: MatSort;
+    @ViewChild(MatSort) sort!: MatSort
     @ViewChild(MatTable) table!: MatTable<MyTableItem>;
-    dataSource: MyTableDataSource;
+    dataSource: MyTableDataSource
 
     /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
     displayedColumns = ['id', 'name'];
