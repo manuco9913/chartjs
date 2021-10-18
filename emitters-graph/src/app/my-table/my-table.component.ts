e8c6bb6a4d71105de210634f1acbc1589477f594
Manu <manu.co9913@gmail.com>
2021-10-18 11:07:10 +0300

    yalla mechadash

--- emitters-graph/src/app/my-table/my-table.component.ts
+++ emitters-graph/src/app/my-table/my-table.component.ts
@@ -4,11 +4,16 @@ import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { MyTableDataSource, MyTableItem } from './my-table-datasource';
 
-@Component({selector: 'app-my-table',templateUrl: './my-table.component.html',styleUrls: ['./my-table.component.css'],})export class MyTableComponent implements AfterViewInit {
+@Component({
+    selector: 'app-my-table',
+    templateUrl: './my-table.component.html',
+    styleUrls: ['./my-table.component.css'],
+})
+export class MyTableComponent implements AfterViewInit {
     @ViewChild(MatPaginator) paginator!: MatPaginator;
-    @ViewChild(MatSort) sort!: MatSort
-    @ViewChild(MatTable) table!: MatTable<MyTableItem>
-    dataSource: MyTableDataSource
+    @ViewChild(MatSort) sort!: MatSort;
+    @ViewChild(MatTable) table!: MatTable<MyTableItem>;
+    dataSource: MyTableDataSource;
 
     /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
     displayedColumns = ['id', 'name'];
